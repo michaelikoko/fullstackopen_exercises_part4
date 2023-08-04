1532b96593f0a12fa5e6ba1f67602ed4ab8bb443
Michael Ikoko <michaelikoko.o@gmail.com>
2023-08-04 19:20:06 +0100

    Added controllers/testing.js

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,6 @@
 const blogsRouter = require('express').Router()
-//const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const middleware = require('../utils/middleware')
-//const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -33,7 +31,8 @@ blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
-    response.status(201).json(savedBlog)
+    const populatedBlog = await savedBlog.populate('user', { username: 1, name: 1 })
+    response.status(201).json(populatedBlog)
 })
 
 blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
@@ -52,29 +51,25 @@ blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) =
 
 })
 
-
-blogsRouter.put('/:id', middleware.userExtractor, async (request, response) => {
+blogsRouter.put('/:id', async (request, response) => {
     const body = request.body
-    const user = request.user
     const blog = await Blog.findById(request.params.id)
 
     if (blog === null) {
         response.status(404).end()
     }
 
-    if (blog.user.toString() === user.id.toString()) {
-        const blog = {
-            'title': body.title,
-            'author': body.author,
-            'url': body.url,
-            'likes': body.likes
-        }
-
-        const updatedNote = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-        response.json(updatedNote)
+    const blogObject = {
+        'title': body.title,
+        'author': body.author,
+        'url': body.url,
+        'likes': body.likes,
+        'user': body.user
     }
-    response.status(401).end()
 
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blogObject, { new: true })
+    const populatedBlog = await updatedBlog.populate('user', { username: 1, name: 1 })
+    response.json(populatedBlog)
 })
 
 module.exports = blogsRouter
